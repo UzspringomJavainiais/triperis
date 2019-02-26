ce5530113080942318302e2046f7ea95ac6996ce
joklek <31686813+joklek@users.noreply.github.com>
2019-02-26 14:12:52 +0200

    Add initializr files

---
+++ src/main/java/com/javainiaisuzspringom/tripperis/TripperIsApplication.java
@@ -0,0 +1,13 @@
+package com.javainiaisuzspringom.tripperis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripperIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripperIsApplication.class, args);
+	}
+
+}
