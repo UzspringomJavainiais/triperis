4af74c9da3af7065c49655392cbd37680d01844c
Paulius Kalvaitis <pauliuskalv@gmail.com>
2019-04-28 16:25:40 +0300

    Add preliminary trip merging functionality
    Signed-off-by: Paulius Kalvaitis <pauliuskalv@gmail.com>

--- src/main/java/com/javainiaisuzspringom/tripperis/controllers/TripController.java
+++ src/main/java/com/javainiaisuzspringom/tripperis/controllers/TripController.java
@@ -1,5 +1,8 @@
 package com.javainiaisuzspringom.tripperis.controllers;
 
+import com.javainiaisuzspringom.tripperis.controllers.util.MergeTrips;
+import com.javainiaisuzspringom.tripperis.domain.Account;
+import com.javainiaisuzspringom.tripperis.domain.ChecklistItem;
 import com.javainiaisuzspringom.tripperis.domain.Trip;
 import com.javainiaisuzspringom.tripperis.services.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class TripController {
@@ -27,5 +31,35 @@ public class TripController {
     public ResponseEntity<Trip> addTrip(@RequestBody Trip trip) {
         Trip savedEntity = tripService.save(trip);
         return new ResponseEntity<>(savedEntity, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/tripMerge")
+    public ResponseEntity<Trip> mergeTrips(@RequestBody MergeTrips mergeTrips) {
+        Trip mergedTrip = new Trip();
+
+        mergedTrip.setName(mergeTrips.getName());
+        mergedTrip.setDescription(mergedTrip.getDescription());
+
+        // Add distinct accounts to the merged trip
+        mergedTrip.setAccounts(mergeTrips.getTripOne().getAccounts());
+
+        for (Account account : mergeTrips.getTripTwo().getAccounts()) {
+            if (!mergedTrip.getAccounts().contains(account))
+                mergedTrip.getAccounts().add(account);
+        }
+
+        // Merge distinct checklist items
+        mergedTrip.setItems(mergeTrips.getTripOne().getItems());
+
+        for (ChecklistItem item : mergeTrips.getTripTwo().getItems()) {
+            if (mergedTrip.getItems().contains(item))
+                mergedTrip.getItems().add(item);
+        }
+
+        // TODO: mergedTrip.setStatus();
+
+        tripService.save(mergedTrip);
+
+        return new ResponseEntity<>(mergedTrip, HttpStatus.CREATED);
     }
 }
