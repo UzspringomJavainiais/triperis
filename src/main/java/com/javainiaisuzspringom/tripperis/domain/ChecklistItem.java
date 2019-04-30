0214edc41d55928106e773be0f72eb3c4308eeb0
Rokas Ulickas <rokas@hcm.lt>
2019-04-30 19:39:12 +0300

    add price to check list item

--- src/main/java/com/javainiaisuzspringom/tripperis/domain/ChecklistItem.java
+++ src/main/java/com/javainiaisuzspringom/tripperis/domain/ChecklistItem.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 @Entity
 @Getter
@@ -20,6 +21,9 @@ public class ChecklistItem implements Serializable {
 
     @Column(name = "IS_CHECKED")
     private boolean isChecked;
+
+    @Column(name = "PRICE")
+    private BigDecimal price;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "trip_id")
