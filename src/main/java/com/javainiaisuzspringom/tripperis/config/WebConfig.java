8448cb5b6d8f475b88251a69b81f96587dd06fb9
joklek <31686813+joklek@users.noreply.github.com>
2019-05-11 23:00:15 +0300

    Drop deprecated class usage

--- src/main/java/com/javainiaisuzspringom/tripperis/config/WebConfig.java
+++ src/main/java/com/javainiaisuzspringom/tripperis/config/WebConfig.java
@@ -3,10 +3,9 @@ package com.javainiaisuzspringom.tripperis.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer  {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
