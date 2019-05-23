781e9079e0166d24702c1d9d8190cfa2834fb4c2
joklek <jokubas.lekevicius@gmail.com>
2019-05-23 09:22:58 +0300

    unfinished changes

--- src/main/java/com/javainiaisuzspringom/tripperis/repositories/RoomUsageRepository.java
+++ src/main/java/com/javainiaisuzspringom/tripperis/repositories/RoomUsageRepository.java
@@ -1,11 +1,11 @@
 package com.javainiaisuzspringom.tripperis.repositories;
 
-import com.javainiaisuzspringom.tripperis.domain.Account;
 import com.javainiaisuzspringom.tripperis.domain.RoomUsage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -13,8 +13,7 @@ public interface RoomUsageRepository extends JpaRepository<RoomUsage, Integer> {
 
     @Query("SELECT ru FROM RoomUsage ru " +
                 "LEFT JOIN ru.apartmentUsage au " +
-            "WHERE ((au.from >= :periodStart AND au.to <= :periodEnd) " +
-                "OR (au.to > :periodStart AND au.from < :periodEnd)) " +
-            "AND ru = :usage")
-    public List<Account> findAllUsersUsingRoomInPeriod(RoomUsage usage);
+            "WHERE ((au.from >= :from AND au.to <= :to) " +
+                "OR (au.to > :from AND au.from < :to))")
+    public List<RoomUsage> findAllRoomUsagesBetweenDates(Timestamp from, Timestamp to);
 }
--- src/main/java/com/javainiaisuzspringom/tripperis/services/RoomUsageService.java
+++ src/main/java/com/javainiaisuzspringom/tripperis/services/RoomUsageService.java
@@ -1,14 +1,21 @@
 package com.javainiaisuzspringom.tripperis.services;
 
+import com.javainiaisuzspringom.tripperis.domain.ApartmentUsage;
 import com.javainiaisuzspringom.tripperis.domain.RoomUsage;
+import com.javainiaisuzspringom.tripperis.dto.TripDuration;
 import com.javainiaisuzspringom.tripperis.dto.entity.RoomUsageDTO;
 import com.javainiaisuzspringom.tripperis.repositories.AccountRepository;
 import com.javainiaisuzspringom.tripperis.repositories.RoomRepository;
 import com.javainiaisuzspringom.tripperis.repositories.RoomUsageRepository;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -38,9 +45,49 @@ public class RoomUsageService implements BasicDtoToEntityService<RoomUsage, Room
         return entity;
     }
 
-    public int getFreeSpaces(RoomUsage roomUsage) {
-        Integer maxCapacity = roomUsage.getRoom().getMaxCapacity();
-        Integer usersInPeriod = repository.findAllUsersUsingRoomInPeriod(roomUsage);
-        return maxCapacity - usersInPeriod;
+    public boolean isAvailableForUsage(RoomUsage usage) {
+        Integer maxCapacity = usage.getRoom().getMaxCapacity();
+        int peopleCount = usage.getAccounts().size();
+        if(peopleCount > maxCapacity) {
+            return false;
+        }
+        Timestamp from = usage.getApartmentUsage().getFrom();
+        Timestamp to = usage.getApartmentUsage().getTo();
+
+        List<RoomUsage> usagesInPeriod = repository.findAllRoomUsagesBetweenDates(from, to);
+        if(usagesInPeriod.isEmpty()) {
+            return true;
+        }
+        Map<Duration, RoomUsage> durationToUsage = usagesInPeriod.stream()
+                .collect(Collectors.toMap(x -> new Duration(x.getApartmentUsage().getFrom(), x.getApartmentUsage().getTo()), Function.identity()));
+        new ArrayList<>(durationToUsage.keySet()).sort(Comparator.naturalOrder());
+        return ;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    private static class Duration implements Comparable<Duration>{
+        Timestamp from;
+        Timestamp to;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Duration duration = (Duration) o;
+            return from.equals(duration.from) &&
+                    to.equals(duration.to);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(from, to);
+        }
+
+        @Override
+        public int compareTo(Duration d) {
+            return this.from.compareTo(d.getFrom());
+        }
     }
 }
