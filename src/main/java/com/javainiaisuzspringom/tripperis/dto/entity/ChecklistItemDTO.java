e38870bafc831cabb6da8758a01ecb68af598f29
joklek <jokubas.lekevicius@gmail.com>
2019-05-29 17:41:45 +0300

    Use lombok

--- src/main/java/com/javainiaisuzspringom/tripperis/dto/entity/ChecklistItemDTO.java
+++ src/main/java/com/javainiaisuzspringom/tripperis/dto/entity/ChecklistItemDTO.java
@@ -1,18 +1,16 @@
 package com.javainiaisuzspringom.tripperis.dto.entity;
 
 import com.javainiaisuzspringom.tripperis.domain.ChecklistItem;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.validation.constraints.Size;
 
 /**
  * Maps to {@link ChecklistItem}
  */
+@Data
 public class ChecklistItemDTO implements ConvertableDTO<Integer>{
 
-    @Getter
-    @Setter
     private Integer id;
 
     @Size(max = 100)
@@ -21,31 +19,4 @@ public class ChecklistItemDTO implements ConvertableDTO<Integer>{
     private boolean isChecked;
 
     private AttachmentDTO attachment;
-
-    public String getName() {
-        return name;
-    }
-
-    public ChecklistItemDTO setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public boolean isChecked() {
-        return isChecked;
-    }
-
-    public ChecklistItemDTO setChecked(boolean checked) {
-        isChecked = checked;
-        return this;
-    }
-
-    public AttachmentDTO getAttachment() {
-        return attachment;
-    }
-
-    public ChecklistItemDTO setAttachment(AttachmentDTO attachment) {
-        this.attachment = attachment;
-        return this;
-    }
 }
